ac8dbe206d5c2d760e18bb6718ff8f9629458ff1
ryanbarnett04 <126820775+ryanbarnett04@users.noreply.github.com>
2026-05-06 01:33:35 +0100

    Added Speed Up status effect

---
+++ StatusEffects/speed_up.py
@@ -0,0 +1,40 @@
+from status_effect import *
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from event import Event
+
+class SpeedUp(StatusEffect):
+
+    def __init__(self, Applicant: "Character", Duration: int, Dispellable: bool, Resistable: bool,
+                 Preventable: bool, Copyable: bool):
+        super().__init__(Applicant, Duration, Dispellable, Resistable, Preventable, Copyable)
+        self.Name = "Speed Up"
+        self.Type = EffectType.Buff
+        self.Description = '''
+        +25% Speed 
+        '''
+
+        self.InitialiseEffect()
+
+    def InitialiseEffect(self):
+
+        if not "SPEED_UP" in self.Applicant.EffectTags:
+            self.Applicant.SetPercentileOffenceModifier(0.25)
+
+        self.Applicant.EffectTags.append("SPEED_UP")
+
+        self.Applicant.Events.RegisteredEffects.append(self)
+
+    def DestroyEffect(self):
+
+        self.Applicant.EffectTags.remove("SPEED_UP")
+
+        if not "SPEED_UP" in self.Applicant.EffectTags:
+            self.Applicant.SetPercentileOffenceModifier(-0.25)
+
+        self.Applicant.Events.RegisteredEffects.remove(self)
+        self.Applicant.StatusEffects.remove(self)
+
+    def Listener(self, event: "Event"):
+        pass
