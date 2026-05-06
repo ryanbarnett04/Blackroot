3241aad570d08dbea577b6f917adbfaa12538f2a
ryanbarnett04 <126820775+ryanbarnett04@users.noreply.github.com>
2026-05-06 01:29:07 +0100

    Added single target heal event

---
+++ Events/heal_instance_single.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING
+from event import EventType, Event
+
+if TYPE_CHECKING:
+    from character import Character
+
+class HealInstanceSingle(Event):
+
+    Healer: "Character"
+    Receiver: "Character"
+    HealthRecovered: int
+
+    def __init__(self, GeneratedBy: "Character", Receiver: "Character", HealthRecovered: int):
+        super().__init__(EventType.DamageInstanceSingle, GeneratedBy)
+
+        self.Healer = self.GeneratedBy
+        self.Receiver = Receiver
+        self.HealthRecovered = HealthRecovered
