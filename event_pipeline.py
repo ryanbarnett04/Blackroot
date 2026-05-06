e0a8e8c3a7a3c25884a114af53c76624b877a523
ryanbarnett04 <126820775+ryanbarnett04@users.noreply.github.com>
2026-05-06 01:28:44 +0100

    Event pipeline can now have status effects hook into alongside abilities

--- event_pipeline.py
+++ event_pipeline.py
@@ -5,15 +5,21 @@ from event import Event
 
 if TYPE_CHECKING:
     from ability import Ability
+    from status_effect import StatusEffect
 
 class EventPipeline:
 
     RegisteredAbilities: list["Ability"]
+    RegisteredEffects: list["StatusEffect"]
 
     def __init__(self):
         self.RegisteredAbilities = []
+        self.RegisteredEffects = []
 
     def DistributeEvent(self, event: Event):
 
         for ability in self.RegisteredAbilities:
             ability.Listener(event)
+
+        for effect in self.RegisteredEffects:
+            effect.Listener(event)
